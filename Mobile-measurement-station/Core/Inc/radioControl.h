d7ceb77d882fc21adbf51aa97b9b7c9e4e6df14a
Szymon Wisniewski <sz.wisniewsky@gmail.com>
2024-11-13 12:20:29 +0100

    add some stuff idk

--- Mobile-measurement-station/Core/Inc/radioControl.h
+++ Mobile-measurement-station/Core/Inc/radioControl.h
@@ -9,9 +9,14 @@
 #define INC_RADIOCONTROL_H_
 
 
-#define FORWARD_BUTTON_Pin LL_GPIO_PIN_0
-#define LEFT_BUTTON_Pin LL_GPIO_PIN_1
+#define FORWARD_BUTTON_Pin LL_GPIO_PIN_1
+#define FORWARD_GPIO GPIOC
+
+#define LEFT_BUTTON_Pin LL_GPIO_PIN_0
+#define LEFT_GPIO GPIOC
+
 #define RIGHT_BUTTON_Pin LL_GPIO_PIN_4
+#define RIGHT_GPIO GPIOA
 
 void init_ControlerButtons(void);
 uint8_t robotComandsSend_test(uint8_t transmitter);
--- Mobile-measurement-station/Core/Src/radioControl.c
+++ Mobile-measurement-station/Core/Src/radioControl.c
@@ -11,21 +11,21 @@
 void init_ControlerButtons(void){
 	LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
 
-	LL_GPIO_SetPinPull(GPIOA, FORWARD_BUTTON_Pin, LL_GPIO_PULL_UP);
-	LL_GPIO_SetPinSpeed(GPIOA, FORWARD_BUTTON_Pin, LL_GPIO_SPEED_FREQ_LOW);
-	LL_GPIO_SetPinMode(GPIOA, FORWARD_BUTTON_Pin, LL_GPIO_MODE_INPUT);
+	LL_GPIO_SetPinPull(FORWARD_GPIO, FORWARD_BUTTON_Pin, LL_GPIO_PULL_UP);
+	LL_GPIO_SetPinSpeed(FORWARD_GPIO, FORWARD_BUTTON_Pin, LL_GPIO_SPEED_FREQ_LOW);
+	LL_GPIO_SetPinMode(FORWARD_GPIO, FORWARD_BUTTON_Pin, LL_GPIO_MODE_INPUT);
 
-	LL_GPIO_SetPinPull(GPIOA, LEFT_BUTTON_Pin, LL_GPIO_PULL_UP);
-	LL_GPIO_SetPinSpeed(GPIOA, LEFT_BUTTON_Pin, LL_GPIO_SPEED_FREQ_LOW);
-	LL_GPIO_SetPinMode(GPIOA, LEFT_BUTTON_Pin, LL_GPIO_MODE_INPUT);
+	LL_GPIO_SetPinPull(LEFT_GPIO, LEFT_BUTTON_Pin, LL_GPIO_PULL_UP);
+	LL_GPIO_SetPinSpeed(LEFT_GPIO, LEFT_BUTTON_Pin, LL_GPIO_SPEED_FREQ_LOW);
+	LL_GPIO_SetPinMode(LEFT_GPIO, LEFT_BUTTON_Pin, LL_GPIO_MODE_INPUT);
 
-	LL_GPIO_SetPinPull(GPIOA, RIGHT_BUTTON_Pin, LL_GPIO_PULL_UP);
-	LL_GPIO_SetPinSpeed(GPIOA, RIGHT_BUTTON_Pin, LL_GPIO_SPEED_FREQ_LOW);
-	LL_GPIO_SetPinMode(GPIOA, RIGHT_BUTTON_Pin, LL_GPIO_MODE_INPUT);
+	LL_GPIO_SetPinPull(RIGHT_GPIO, RIGHT_BUTTON_Pin, LL_GPIO_PULL_UP);
+	LL_GPIO_SetPinSpeed(RIGHT_GPIO, RIGHT_BUTTON_Pin, LL_GPIO_SPEED_FREQ_LOW);
+	LL_GPIO_SetPinMode(RIGHT_GPIO, RIGHT_BUTTON_Pin, LL_GPIO_MODE_INPUT);
 }
 
 uint8_t robotComandsSend_test(uint8_t transmitter){
-	if (!LL_GPIO_IsInputPinSet(GPIOA, FORWARD_BUTTON_Pin))
+	if (!LL_GPIO_IsInputPinSet(FORWARD_GPIO, FORWARD_BUTTON_Pin))
 	{
 		  uint8_t input = 1;
 		  uint8_t output = 0;
@@ -42,6 +42,7 @@ uint8_t robotComandsSend_test(uint8_t transmitter){
 		  }
 		  return output;
 	}
+	return 0;
 }
 
 
@@ -50,15 +51,15 @@ void robotComandsSend(uint8_t transmitter)
 	uint8_t input = 0;
 	uint8_t size = 1;
 
-	if (!LL_GPIO_IsInputPinSet(GPIOA, FORWARD_BUTTON_Pin))
+	if (!LL_GPIO_IsInputPinSet(FORWARD_GPIO, FORWARD_BUTTON_Pin))
 	{
 		input = 1;  // robot forward
 	}
-	else if (!LL_GPIO_IsInputPinSet(GPIOA, LEFT_BUTTON_Pin))
+	else if (!LL_GPIO_IsInputPinSet(LEFT_GPIO, LEFT_BUTTON_Pin))
 	{
 		input = 2;  // robot turn left
 	}
-	else if (!LL_GPIO_IsInputPinSet(GPIOA, RIGHT_BUTTON_Pin))
+	else if (!LL_GPIO_IsInputPinSet(RIGHT_GPIO, RIGHT_BUTTON_Pin))
 	{
 		input = 3;  // robot turn right
 	}
