56bdee73285f12fc361a6b3d7b8ecc34c92fe76b
wik <ciastko.manak@wp.pl>
2024-11-16 14:18:50 +0100

    nic

---
+++ Mobile-measurement-station/Core/Inc/akcelerometr.h
@@ -0,0 +1,13 @@
+/*
+ * akcelerometr.h
+ *
+ *  Created on: Nov 16, 2024
+ *      Author: ciast
+ */
+
+#ifndef INC_AKCELEROMETR_H_
+#define INC_AKCELEROMETR_H_
+
+
+
+#endif /* INC_AKCELEROMETR_H_ */
