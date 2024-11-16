d2288b8dc1042d3b04996aa53a06a740789643ba
wik <ciastko.manak@wp.pl>
2024-11-16 20:24:00 +0100

    dalej_nic

--- Mobile-measurement-station/Core/Inc/akcelerometr.h
+++ Mobile-measurement-station/Core/Inc/akcelerometr.h
@@ -8,6 +8,16 @@
 #ifndef INC_AKCELEROMETR_H_
 #define INC_AKCELEROMETR_H_
 
+#include "main.h"
+#include "i2c.h"
+
+void EXTI4_15_IRQHandler(void);
+void init_ControlerButtons(void);
+
+
+#define GPIO_AKCELEROMETR_PRZERWANIE_PIN LL_GPIO_PIN_6
+#define GPIO_AKCELEROMETR_PRZERWANIE GPIOA
+
 
 
 #endif /* INC_AKCELEROMETR_H_ */
--- Mobile-measurement-station/Core/Src/akcelerometr.c
+++ Mobile-measurement-station/Core/Src/akcelerometr.c
@@ -4,5 +4,40 @@
  *  Created on: Nov 16, 2024
  *      Author: ciast
  */
+#include "akcelerometr.h"
+#include "main.h"
+#include "i2c.h"
+
+void init_Akcelerometr(void){
+
+	LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+
+
+	LL_GPIO_SetPinPull(GPIO_AKCELEROMETR_PRZERWANIE, GPIO_AKCELEROMETR_PRZERWANIE_PIN, LL_GPIO_PULL_DOWN);
+	LL_GPIO_SetPinSpeed(GPIO_AKCELEROMETR_PRZERWANIE, GPIO_AKCELEROMETR_PRZERWANIE_PIN, LL_GPIO_SPEED_FREQ_LOW);
+	LL_GPIO_SetPinMode(GPIO_AKCELEROMETR_PRZERWANIE, GPIO_AKCELEROMETR_PRZERWANIE_PIN, LL_GPIO_MODE_INPUT);
+
+	LL_EXTI_SetEXTISource(GPIO_AKCELEROMETR_PRZERWANIE, GPIO_AKCELEROMETR_PRZERWANIE_PIN);
+
+	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_6);
+
+	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_6);
+
+	NVIC_SetPriority(EXTI9_5_IRQn, 0);
+	NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+
+	I2C1_reg_write_it(0X0F, 0X30, 127, 1);
+}
+
+void EXTI4_15_IRQHandler(void)
+{
+	if(LL_EXTI_IsActiveFallingFlag_0_31(LL_EXTI_LINE_6) != RESET)
+	{
+		LL_EXTI_ClearFallingFlag_0_31(LL_EXTI_LINE_6);
+	}
+}
+
+
 
 
